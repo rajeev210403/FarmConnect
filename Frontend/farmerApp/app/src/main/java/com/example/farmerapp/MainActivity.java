212cc670c74df0e20be78a751d169a34632c4392
GeeksforGeeks <110807377+rajeev210403@users.noreply.github.com>
2023-08-21 19:51:58 +0530

    home page

--- Frontend/farmerApp/app/src/main/java/com/example/farmerapp/MainActivity.java
+++ Frontend/farmerApp/app/src/main/java/com/example/farmerapp/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.farmerapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.farmerapp.Adaptor.CategoryAdaptor;
 import com.example.farmerapp.Adaptor.TopProductAdaptor;
