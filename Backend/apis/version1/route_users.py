38cd15f5d9987a7a4ed6fc0550714dfad96431e8
honestfreak <me21b006@iittp.ac.in>
2023-08-22 01:23:40 +0530

    signup

--- Backend/apis/version1/route_users.py
+++ Backend/apis/version1/route_users.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import Session
 router = APIRouter()
 
 
-@router.post("/", response_model=ShowUser)
+@router.post("/")
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
     user = create_new_user(user=user, db=db)
-    return user
+    return {"message" : "inserted"}
