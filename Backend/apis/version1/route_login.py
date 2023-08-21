17256d4068ee72586f6a5def08b8dcd5d1e3bc0f
honestfreak <me21b006@iittp.ac.in>
2023-08-22 00:43:47 +0530

    login

--- Backend/apis/version1/route_login.py
+++ Backend/apis/version1/route_login.py
@@ -32,7 +32,7 @@ def authenticate_user(username: str, password: str, db: Session = Depends(get_db
         return False
     return user
 
-@router.post("/token", response_model=Token)
+@router.post("/token")
 def login_for_access_token(
     response: Response,
     form_data: OAuth2PasswordRequestForm = Depends(),
@@ -40,19 +40,8 @@ def login_for_access_token(
 ):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-        )
-    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.email}, expires_delta=access_token_expires
-    )
-    response.set_cookie(
-        key="access_token", value=f"Bearer {access_token}", httponly=True
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
-
+        return {"message": "invalid"}
+    return {"message": "valid"}
 
 oauth2_scheme = OAuth2PasswordBearerWithCookie(tokenUrl="/login/token")
 
