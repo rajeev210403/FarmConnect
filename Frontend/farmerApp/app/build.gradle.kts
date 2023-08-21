fa9efa0613d916493d9cd65ed162fb8eb2d9f17b
GeeksforGeeks <110807377+rajeev210403@users.noreply.github.com>
2023-08-21 19:32:40 +0530

    gradle

--- Frontend/farmerApp/app/build.gradle.kts
+++ Frontend/farmerApp/app/build.gradle.kts
@@ -33,10 +33,21 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-
+    implementation("com.airbnb.android:lottie:3.7.0")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.5.0")
 
+
+    //newly added
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+
+
+    //prev
     testImplementation("junit:junit:4.13.2")
+//    implementation ('com.github.bumptech.glide:glide:4.8.0'
+//    annotationProcessor 'com.github.bumptech.glide:compiler:4.8.0'
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
