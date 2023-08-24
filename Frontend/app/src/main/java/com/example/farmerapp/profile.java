7cbb8a058d02dff98ce7d0fba92a6adb7e3c03e3
SaiVignesh-K <Komminenisaivignesh.2003@gmail.com>
2023-08-24 07:27:45 +0530

    profil

--- Frontend/app/src/main/java/com/example/farmerapp/profile.java
+++ Frontend/app/src/main/java/com/example/farmerapp/profile.java
@@ -1,14 +1,25 @@
 package com.example.farmerapp;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class profile extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+public class profile extends AppCompatActivity {
+    Button signout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        signout = findViewById(R.id.signout);
+        signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(profile.this, signup.class);
+                startActivity(intent);
+            }
+        });
     }
 }
