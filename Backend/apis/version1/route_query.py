3d9254008b79e1ee3576631e59e1dc405e12d829
honestfreak <me21b006@iittp.ac.in>
2023-08-24 08:21:57 +0530

    readme

--- Backend/apis/version1/route_query.py
+++ Backend/apis/version1/route_query.py
@@ -4,15 +4,50 @@ from fastapi import Depends
 from sqlalchemy.orm import Session
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage, SystemMessage
+from chromadb.config import Settings
+import chromadb
 
 router = APIRouter()
 
 
 @router.get("/query={query}")
 def query(query: str, db: Session = Depends(get_db)):
-    chat = ChatOpenAI(openai_api_key="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"
+    chat = ChatOpenAI(openai_api_key="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"
                       , model="gpt-3.5-turbo-0613")
     messages = [HumanMessage(content= query)]
     ans = chat(messages).content
 
     return ans
+
+def ask(api,query,query1,query2,c,model,bot_id):
+    client = chromadb.Client(Settings(
+    chroma_db_impl="duckdb+parquet",
+    persist_directory="backend/docs"))  
+    collection = client.get_or_create_collection("col" + str(bot_id))
+    res = collection.query(query_texts=[query] ,n_results=2)
+    model = model.split("-")[1]
+    chat = ChatOpenAI(temperature=c,openai_api_key=api['openai'], model="gpt-3.5-turbo-0613")
+    context = ". ".join(res["documents"][0])
+    messages = [HumanMessage(content=query1 + context + query2)]
+    ans = chat(messages).content
+    print(ans)
+    return ans  
+
+def add_doc(docs, personal_id ,bot_id):
+    char_bot_id = "col"+str(bot_id)
+    client = chromadb.Client(Settings(
+    chroma_db_impl="duckdb+parquet",
+    persist_directory="backend/docs" ))
+            
+    collection = client.get_or_create_collection(char_bot_id) 
+    
+    ids = list(range(personal_id, personal_id+len(docs)))
+    charid = ["id"+str(i) for i in ids]
+    collection.add(documents=docs,ids=charid)
+    personal_id = personal_id+len(docs)
+   
+    print(char_bot_id)
+    print(collection.count())
+    client.persist()
+    
+    return personal_id
--- README.md
+++ README.md
@@ -20,10 +20,6 @@ Designing innovative Android applications to address societal challenges in the 
 8. Multilingual
 
 
-## Technology Stack:
-
-<img src="https://img.shields.io/badge/html5%20-%23E34F26.svg?&style=for-the-badge&logo=html5&logoColor=white"/> <img src="https://img.shields.io/badge/css3%20-%231572B6.svg?&style=for-the-badge&logo=css3&logoColor=white"/> <img src="https://img.shields.io/badge/javascript%20-%23323330.svg?&style=for-the-badge&logo=javascript&logoColor=%23F7DF1E"/> <img src="https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white"/>  <img src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB"/>  <img src="https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&logo=react-router&logoColor=white"/>  <img src="https://img.shields.io/badge/markdown-%23000000.svg?&style=for-the-badge&logo=markdown&logoColor=white"/><img src="https://img.shields.io/badge/github%20-%23121011.svg?&style=for-the-badge&logo=github&logoColor=white"/> 
-
 - **Frontend**: Java
 - **Backend**: 
 - **IDE**: VS Code, Android Studio
